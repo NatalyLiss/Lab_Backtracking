1d8aa0b969e3ae29cf8c1eacbf8c11af30533036
Nataly Liss <natalyliss@mail.ru>
2017-04-12 12:59:54 +0300

    Test commit

--- Backtracking.cpp
+++ Backtracking.cpp
@@ -7,6 +7,9 @@
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
+	cout << " Лабораторная №2. Задача о ферзях." << endl;
+	cout << " Нужно расставить ферзей на доске так, чтобы они не били друг друга. " << endl;
 	cleanQueen();
 	tryQueen(0);
 	printQueen();
